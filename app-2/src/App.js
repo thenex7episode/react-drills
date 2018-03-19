cbc20dbf3d720bc0c4af83847a77a121292a5d62
William <lambermontwilliam@gmail.com>
2018-03-19 13:55:29 -0700

    app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      food: 'lasagna',
+      car: 'lambo',
+      place: 'las vegas'
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h4>{this.state.food}</h4>
+        <h4>{this.state.car}</h4>
+        <h4>{this.state.place}</h4>
       </div>
     );
   }
